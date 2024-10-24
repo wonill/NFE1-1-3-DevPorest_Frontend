92f81e65e5ecb68aa6415e7cd7df346dd3cc9241
Eugeeun <jack603@naver.com>
2024-10-24 17:26:36 +0900

    📝 글로벌 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div>App</div>;
+import React from 'react';
+import { Global, css } from '@emotion/react';
+import { global } from './styles/reset';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Global
+        styles={css`
+          ${global}
+        `}
+      />
+    </div>
+  );
 };
 
 export default App;
