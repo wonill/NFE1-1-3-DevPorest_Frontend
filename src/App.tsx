5400e2d56da5463b90352f8c137b4b8e9e4b9139
Eugeeun <jack603@naver.com>
2024-10-28 12:35:30 +0900

    📝 App컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,29 @@ import DetailPage from "./pages/detail/DetailPage";
 import Register from "./pages/register/Register";
 import EditPortfolioPage from "./pages/edit_portfolio/EditPortfolioPage";
 import EditProfilePage from "./pages/edit_profile/EditProfilePage";
+import MainLayout from "./layouts/MainLayout";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Global
         styles={css`
           ${global}
         `}
       />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/detail" element={<DetailPage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/edit_portfolio" element={<EditPortfolioPage />} />
-          <Route path="/edit_profile" element={<EditProfilePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="detail" element={<DetailPage />} />
+          <Route path="edit_portfolio" element={<EditPortfolioPage />} />
+        </Route>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<Register />} />
+        <Route path="edit_profile" element={<EditProfilePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
