d210bc97a3274d6a1cd194d0b109bb7411d48a0b
Eugeeun <jack603@naver.com>
2024-10-29 01:06:40 +0900

    💄 입력 필드 스타일 분리

--- src/pages/EditProfile/EditProfilePage.styles.ts
+++ src/pages/EditProfile/EditProfilePage.styles.ts
@@ -83,36 +83,6 @@ export const UserInfoInputWrapper = styled.div`
   }
 `;
 
-export const UserInfoInput = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: ${({ theme }) => theme.PADDINGS.X_SMALL};
-`;
-
-export const IconWrapper = styled.div`
-  width: 30px;
-  height: 100%;
-
-  img {
-    width: 100%;
-    height: 100%;
-  }
-`;
-
-export const Input = styled.input`
-  width: 100%;
-  padding: ${({ theme }) => `calc(${theme.PADDINGS.X_SMALL} / 2)`};
-  border-radius: ${({ theme }) => theme.BORDER_RADIUS.DEFAULT};
-  border: 1px solid ${({ theme }) => theme.COLORS.MAIN_GRAY};
-  color: ${({ theme }) => theme.COLORS.MAIN_BLACK};
-  font-size: ${({ theme }) => theme.FONT_SIZE.CONTENT};
-
-  &:focus {
-    outline: 1px solid ${({ theme }) => theme.COLORS.MAIN_GREEN};
-  }
-`;
-
 export const RightUserInfo = styled.div`
   flex: 7;
   display: flex;
