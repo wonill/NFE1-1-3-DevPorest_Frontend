81500a084e4263d815ff7f6f78c6b9efba8b29fa
Eugeeun <jack603@naver.com>
2024-10-30 16:14:56 +0900

    📝 Like api 타입 추가

---
+++ src/types/Like.ts
@@ -0,0 +1,11 @@
+/**
+ * --------------------------------------------------
+ * 좋아요 추가/취소: POST
+ * /api/portfolios/:id/like
+ * --------------------------------------------------
+ */
+export interface Like {
+  like?: boolean; // 성공시 좋아요 여부
+  likeCount?: number; // 성공시 좋아요 수
+  message?: string; // 실패시 에러 메세지
+}
