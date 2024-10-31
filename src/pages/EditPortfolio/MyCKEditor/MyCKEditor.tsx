3fbf58082d376594222c265fdcdb32950822c295
jooyoung <jykimjbr860@gmail.com>
2024-10-31 14:59:30 +0900

    ♻️ Image 입력 가능(base64UploadAdapter)

--- src/pages/EditPortfolio/MyCKEditor/MyCKEditor.tsx
+++ src/pages/EditPortfolio/MyCKEditor/MyCKEditor.tsx
@@ -8,9 +8,9 @@ import {
   Autoformat,
   AutoImage,
   Autosave,
+  Base64UploadAdapter,
   BlockQuote,
   Bold,
-  CloudServices,
   Essentials,
   Heading,
   ImageBlock,
@@ -35,7 +35,6 @@ import {
   Paragraph,
   PasteFromOffice,
   SelectAll,
-  SimpleUploadAdapter,
   Table,
   TableCaption,
   TableCellProperties,
@@ -46,10 +45,10 @@ import {
   TodoList,
   Underline,
   Undo,
+  FullPage,
 } from "ckeditor5";
 
 import translations from "ckeditor5/translations/ko.js";
-
 import "ckeditor5/ckeditor5.css";
 
 const MyCKEditor = () => {
@@ -94,10 +93,11 @@ const MyCKEditor = () => {
       Autoformat,
       AutoImage,
       Autosave,
+      Base64UploadAdapter,
       BlockQuote,
       Bold,
-      CloudServices,
       Essentials,
+      FullPage,
       Heading,
       ImageBlock,
       ImageCaption,
@@ -121,7 +121,6 @@ const MyCKEditor = () => {
       Paragraph,
       PasteFromOffice,
       SelectAll,
-      SimpleUploadAdapter,
       Table,
       TableCaption,
       TableCellProperties,
@@ -137,45 +136,44 @@ const MyCKEditor = () => {
       options: [
         {
           model: "paragraph" as const,
-          view: "p",
-          title: "Paragraph",
-          class: "ck-heading_paragraph",
+          title: "Paragraph" as const,
+          class: "ck-heading_paragraph as const",
         },
         {
           model: "heading1" as const,
-          view: "h1",
-          title: "Heading 1",
-          class: "ck-heading_heading1",
+          view: "h1" as const,
+          title: "Heading 1" as const,
+          class: "ck-heading_heading1" as const,
         },
         {
           model: "heading2" as const,
-          view: "h2",
-          title: "Heading 2",
-          class: "ck-heading_heading2",
+          view: "h2" as const,
+          title: "Heading 2" as const,
+          class: "ck-heading_heading2" as const,
         },
         {
           model: "heading3" as const,
-          view: "h3",
-          title: "Heading 3",
-          class: "ck-heading_heading3",
+          view: "h3" as const,
+          title: "Heading 3" as const,
+          class: "ck-heading_heading3" as const,
         },
         {
           model: "heading4" as const,
-          view: "h4",
-          title: "Heading 4",
-          class: "ck-heading_heading4",
+          view: "h4" as const,
+          title: "Heading 4" as const,
+          class: "ck-heading_heading4" as const,
         },
         {
           model: "heading5" as const,
-          view: "h5",
-          title: "Heading 5",
-          class: "ck-heading_heading5",
+          view: "h5" as const,
+          title: "Heading 5" as const,
+          class: "ck-heading_heading5" as const,
         },
         {
           model: "heading6" as const,
-          view: "h6",
-          title: "Heading 6",
-          class: "ck-heading_heading6",
+          view: "h6" as const,
+          title: "Heading 6" as const,
+          class: "ck-heading_heading6" as const,
         },
       ],
     },
@@ -191,14 +189,14 @@ const MyCKEditor = () => {
         "resizeImage",
       ],
     },
-    initialData: "",
-    language: "ko",
+    initialData:
+      '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
     link: {
       addTargetToExternalLinks: true,
       defaultProtocol: "https://",
       decorators: {
         toggleDownloadable: {
-          mode: "manual" as const, // 'manual'을 const로 지정
+          mode: "manual" as const,
           label: "Downloadable",
           attributes: {
             download: "file",
@@ -213,7 +211,7 @@ const MyCKEditor = () => {
         reversed: true,
       },
     },
-    placeholder: "내용을 입력해주세요.",
+    placeholder: "내용을 입력해주세요!",
     table: {
       contentToolbar: [
         "tableColumn",
