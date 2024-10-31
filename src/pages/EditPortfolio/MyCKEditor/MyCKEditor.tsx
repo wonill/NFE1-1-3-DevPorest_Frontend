703d5b0dd167c998629f2ff3494ee1ef6d7e2223
jooyoung <jykimjbr860@gmail.com>
2024-10-31 16:44:18 +0900

    🐛 Ckeditor 작성 페이지 내 적용 안되는 것 수정

--- src/pages/EditPortfolio/MyCKEditor/MyCKEditor.tsx
+++ src/pages/EditPortfolio/MyCKEditor/MyCKEditor.tsx
@@ -1,13 +1,14 @@
-import "./MyCKEditor.css";
 import { useState, useEffect, useRef } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+import "./MyCKEditor.css";
+import "ckeditor5/ckeditor5.css";
 
 import {
   ClassicEditor,
   AccessibilityHelp,
   Autoformat,
   AutoImage,
-  Autosave,
   Base64UploadAdapter,
   BlockQuote,
   Bold,
@@ -15,14 +16,11 @@ import {
   Heading,
   ImageBlock,
   ImageCaption,
-  ImageInline,
   ImageInsert,
   ImageInsertViaUrl,
   ImageResize,
   ImageStyle,
   ImageTextAlternative,
-  ImageToolbar,
-  ImageUpload,
   Indent,
   IndentBlock,
   Italic,
@@ -32,24 +30,18 @@ import {
   ListProperties,
   Markdown,
   MediaEmbed,
-  Paragraph,
   PasteFromOffice,
-  SelectAll,
   Table,
   TableCaption,
   TableCellProperties,
   TableColumnResize,
   TableProperties,
   TableToolbar,
-  TextTransformation,
   TodoList,
   Underline,
-  Undo,
-  FullPage,
 } from "ckeditor5";
 
 import translations from "ckeditor5/translations/ko.js";
-import "ckeditor5/ckeditor5.css";
 
 const MyCKEditor = () => {
   const editorContainerRef = useRef(null);
@@ -92,23 +84,18 @@ const MyCKEditor = () => {
       AccessibilityHelp,
       Autoformat,
       AutoImage,
-      Autosave,
       Base64UploadAdapter,
       BlockQuote,
       Bold,
       Essentials,
-      FullPage,
       Heading,
       ImageBlock,
       ImageCaption,
-      ImageInline,
       ImageInsert,
       ImageInsertViaUrl,
       ImageResize,
       ImageStyle,
       ImageTextAlternative,
-      ImageToolbar,
-      ImageUpload,
       Indent,
       IndentBlock,
       Italic,
@@ -118,19 +105,15 @@ const MyCKEditor = () => {
       ListProperties,
       Markdown,
       MediaEmbed,
-      Paragraph,
       PasteFromOffice,
-      SelectAll,
       Table,
       TableCaption,
       TableCellProperties,
       TableColumnResize,
       TableProperties,
       TableToolbar,
-      TextTransformation,
       TodoList,
       Underline,
-      Undo,
     ],
     heading: {
       options: [
@@ -190,7 +173,7 @@ const MyCKEditor = () => {
       ],
     },
     initialData:
-      '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
+      "<h2>포트폴리오 생성 공간에 오신 것을 환영합니다🎉</h2>\n<p>나만의 포트폴리오를 만들어보세요</p>  ",
     link: {
       addTargetToExternalLinks: true,
       defaultProtocol: "https://",
