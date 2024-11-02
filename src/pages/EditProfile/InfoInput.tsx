706a4d0cd0231f72e0547b4b3f8b4fb2602f6ef0
Eugeeun <jack603@naver.com>
2024-11-02 16:44:10 +0900

    ✨ 에러 기능 추가

--- src/pages/EditProfile/InfoInput.tsx
+++ src/pages/EditProfile/InfoInput.tsx
@@ -4,14 +4,17 @@ import { UserInfoInput, IconWrapper, Input } from "./InfoInput.styles";
 interface UserInfoInputProps {
   icon: string;
   placeholder: string;
+  value: string;
+  error?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InfoInput: React.FC<UserInfoInputProps> = ({ icon, placeholder }) => (
+const InfoInput: React.FC<UserInfoInputProps> = ({ icon, placeholder, value, error, onChange }) => (
   <UserInfoInput>
     <IconWrapper>
       <img src={icon} alt="" />
     </IconWrapper>
-    <Input type="text" placeholder={placeholder} />
+    <Input type="text" placeholder={placeholder} value={value} onChange={onChange} error={error} />
   </UserInfoInput>
 );
 
