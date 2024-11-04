9b6b607505a8cac778d15a58ebe34ed7403f77d0
wonil <nwi6959@gmail.com>
2024-11-05 07:11:50 +0900

    ✨  인기 포트폴리오 섹션 API 연동

--- src/pages/main/PopularPortfolioSection.tsx
+++ src/pages/main/PopularPortfolioSection.tsx
@@ -1,16 +1,51 @@
+import {useState, useEffect} from 'react';
 import { StyledPopularPortfolioSection } from "./PopularPortfolioSection.style";
 import PopularPortfolio from "../../components/PopularPortfolio/PopularPortfolio";
 
-import { popularPortfolioData } from "../../data/mainPageData";
+import { getPortfolios } from "../../api/get-portfolios";
+import { DetailPortfolioType } from "../../types/api-types/PortfolioType";
+import { useNavigate } from 'react-router-dom';
+
+const searchParams = new URLSearchParams({
+  jobGroup: 'all',
+  techStacks: '',
+  searchType: 'title',
+  keyword: '',
+  sort: 'likes',
+  page: '1',
+  limit: '3'
+}).toString();
 
 const PopularPortfolioSection = () => {
+  const navigate = useNavigate();
+
+  const [popularPortfolioList, setPopularPortfolioList] = useState<DetailPortfolioType[]>([]);
+
+  useEffect(() => {
+    const fetchPortfolios = async () => {
+      try {
+        const result = await getPortfolios(searchParams);
+        if (result) {
+          console.log(result.data);
+          setPopularPortfolioList(result.data);
+        }
+      } catch (error) {
+        console.error('포트폴리오 불러오기 실패:', error);
+      }
+    };
+    fetchPortfolios();
+  }, []);
   return (
     <StyledPopularPortfolioSection>
-      {popularPortfolioData.map((portfolio) => (
+      {popularPortfolioList.map((portfolio) => (
         <PopularPortfolio
-          key={portfolio.id}
-          {...portfolio}
-          onClick={() => console.log(portfolio.id)}
+          key={portfolio._id}
+          id={portfolio._id}
+          image={portfolio.thumbnailImage}
+          title={portfolio.title}
+          name={portfolio.userInfo.name}
+          job={portfolio.jobGroup!}
+          onClick={() => navigate(`detail?portfolio_id=${portfolio._id}`)}
         />
       ))}
     </StyledPopularPortfolioSection>
