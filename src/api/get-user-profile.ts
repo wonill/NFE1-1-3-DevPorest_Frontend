d14cb3931ee4b94d536672689d183e9fe64e4c81
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-05 14:49:12 +0900

    ♻️ get-user-profile 리팩토링 ( api instance 사용 )

--- src/api/get-user-profile.ts
+++ src/api/get-user-profile.ts
@@ -1,14 +1,14 @@
-import ky from "ky";
+import api from ".";
 import { UserProfileResType, UserApiResType } from "../types/api-types/UserType";
-const apiUrl = import.meta.env.VITE_SERVER_URL;
 
 export const getUserProfile = async (userId: string) => {
   try {
-    const response = await ky.get(`${apiUrl}/users/user/${userId}`, { credentials: "include" });
-    const result: UserApiResType<UserProfileResType> = await response.json();
+    const response = await api
+      .get<UserApiResType<UserProfileResType>>(`users/user/${userId}`)
+      .json();
 
-    if (result.success) return result.data;
-    else return result.error;
+    if (response.success) return response.data;
+    else return response.error;
   } catch (error) {
     console.log(error);
     return undefined;
@@ -17,12 +17,11 @@ export const getUserProfile = async (userId: string) => {
 
 export const getPopularUserProfile = async () => {
   try {
-    const response = await ky.get(`${apiUrl}/users/popular`, {credentials:'include'});
-    const result: UserApiResType<UserProfileResType> = await response.json();
-    return result.data;
+    const response = await api.get<UserApiResType<UserProfileResType>>(`users/popular`).json();
 
+    return response.data;
   } catch (error) {
-    console.error('인기 개발자 프로필을 가져오던 중 오류 발생', error);
+    console.error("인기 개발자 프로필을 가져오던 중 오류 발생", error);
     throw error;
   }
-}
+};
