2863b2e86473289e71725801e0df3d0716de4875
wonil <nwi6959@gmail.com>
2024-11-05 09:34:04 +0900

    ✨ 그래프 섹션 데이터 바인딩

--- src/pages/main/GraphSection.tsx
+++ src/pages/main/GraphSection.tsx
@@ -1,4 +1,4 @@
-import Graph from "../../components/Graph/Graph";
+import Graph, { GraphDataType } from "../../components/Graph/Graph";
 import { SectionTitle } from "./MainPage.style";
 
 import { graphData } from "../../data/mainPageData";
@@ -10,8 +10,46 @@ import {
   BackendGraph,
   GraphTitle,
 } from "./GraphSection.style";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { TechStackStatType } from "../../types/api-types/TechStackType";
+import { getTechStackStatistic } from "../../api/get-tech-stacks";
 
 const GraphSection = () => {
+  const navigate = useNavigate();
+  const [techStacks, setTechStacks] = useState<TechStackStatType[]>([]);
+  const [statistics, setStatistics] = useState<GraphDataType[]>([]);
+  const [frontStats, setfrontStats] = useState<GraphDataType[]>([]);
+  const [backendStats, setBackendStats] = useState<GraphDataType[]>([]);
+
+  useEffect(() => {
+    const fetchTechStackList = async () => {
+      const result = await getTechStackStatistic();
+      console.log(result);
+      if (Array.isArray(result)) setTechStacks(result);
+    }
+    fetchTechStackList();
+  }, [])
+
+  useEffect(() => {
+    if (techStacks.length > 0) {
+      const front = techStacks.filter(techStack => techStack.jobCode === '671f9d162a296054c7477856' && techStack.totalCount > 0).map((v) => ({
+        id: v.skill,
+        value: v.totalCount,
+        color: v.bgColor,
+      }));
+      setfrontStats(front);
+      console.log('프론트', front);
+      const back = techStacks.filter(techStack => techStack.jobCode === '672616239a64f518f7c8d530' && techStack.totalCount > 0).map((v) => ({
+        id: v.skill,
+        value: v.totalCount,
+        color: v.bgColor,
+      }));
+      setBackendStats(back);
+      console.log('백엔드', back);
+    }
+  }, [techStacks]); // techStacks가 변경될 때마다 실행
+
   return (
     <StyledGraphSection>
       <SectionTitle>기술 스택별 점유율</SectionTitle>
@@ -21,15 +59,15 @@ const GraphSection = () => {
       <GraphWrapper>
         <FrontendGraph>
           <Graph
-            data={graphData}
-            onClick={(data) => console.log(data.id, data.value)}
+            data={frontStats}
+            onClick={(frontStats) => console.log(frontStats.id, frontStats.value)}
           />
           <GraphTitle>Frontend</GraphTitle>
         </FrontendGraph>
         <BackendGraph>
           <Graph
-            data={graphData}
-            onClick={(data) => console.log(data.id, data.value)}
+            data={backendStats}
+            onClick={(backendStats) => console.log(backendStats.id, backendStats.value)}
           />
           <GraphTitle>Backend</GraphTitle>
         </BackendGraph>
