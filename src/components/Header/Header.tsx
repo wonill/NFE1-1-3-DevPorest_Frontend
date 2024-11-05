d7a3c68487ef1a184ed7d2fb800ba6cf45cec836
jooyoung <jykimjbr860@gmail.com>
2024-11-05 15:24:20 +0900

    ♻️ Header 반응형/로그인/검색바/라우팅
    페이지 크기 반응형/로그인시 프로필보여주기/검색페이지에서 검색바 없애기/로그인과 검색 라우팅 설정해줬습니다

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,195 @@
-import { HeaderWrapper } from "./Header.styles";
+import { HeaderWrapper, SearchBarWrapper } from "./Header.styles";
 import Button from "../Button/Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Logo from "../Logo/Logo";
+import { UserApiResType, UserProfileResType } from "../../types/api-types/UserType";
+import { useEffect, useState } from "react";
+import ky from "ky";
+import noImg from "../../assets/no_image.svg";
+const apiUrl = import.meta.env.VITE_SERVER_URL;
+import useStoreSearchPage from "../../store/store-search-page";
 
-interface HeaderProps {
-  onEnterSearch?: (word: string) => void;
-  onClickLogin?: () => void;
-  onClickRegister?: () => void;
+interface HeaderCompProps {
+  userProfile?: UserProfileResType;
+  isSearchPage: boolean;
+  isSearchOpen: boolean;
+  setIsSearchOpen: (b: boolean) => void;
+  toggleSearch: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({
-  onEnterSearch,
-  onClickLogin,
-  onClickRegister,
-}) => {
+const Header = () => {
+  const [isSearchPage, setIsSearchPage] = useState(false);
+  const [userId, setUserId] = useState<string | undefined>("");
+  const [userProfile, setUserProfile] = useState<UserProfileResType>();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const location = useLocation();
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  useEffect(() => {
+    setIsSearchPage(location.pathname === "/search");
+  }, [useLocation()]);
+
+  useEffect(() => {
+    const fetchLoggedInUser = async () => {
+      try {
+        const response = await ky.get(`${apiUrl}/users/user`);
+        if (!response.ok) {
+          setUserId(undefined);
+          throw new Error("헤더: 유저 정보를 불러오는 데 실패했습니다.");
+        }
+
+        const jsonData: UserApiResType<UserProfileResType> = await response.json();
+        setUserProfile(jsonData.data);
+        setUserId(jsonData.data?.userID);
+        console.log("헤더: ", userId, "로 로그인되어있습니다.");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchLoggedInUser();
+  }, []);
+
   return (
     <HeaderWrapper>
       <Link to="/" className="logo">
-        {/* <i className="logo"> */}
-        <img src="/tree-logo.svg" alt="tree-logo" />
-        <img src="/devporest-logo.svg" alt="DevPorest" />
-        {/* </i> */}
+        <Logo />
       </Link>
-      <div className="wrap">
-        <input
-          type="text"
-          className="searchBar"
-          placeholder="다양한 개발자 포트폴리오 검색"
-          onKeyDown={(ev) => {
-            if (ev.key === "Enter" && onEnterSearch) {
-              onEnterSearch("searchword");
-            }
-          }}
+      {userId ? (
+        <LoggedInHeaderComp
+          userProfile={userProfile}
+          isSearchPage={isSearchPage}
+          isSearchOpen={isSearchOpen}
+          setIsSearchOpen={setIsSearchOpen}
+          toggleSearch={toggleSearch}
         />
-        <img src="/search-icon.svg" className="searchIcon" alt="" />
-        <Button text="로그인" colorType={0} onClick={onClickLogin} />
-        <Button text="회원가입" colorType={3} onClick={onClickRegister} />
-      </div>
+      ) : (
+        <LoggedOutHeaderComp
+          isSearchPage={isSearchPage}
+          isSearchOpen={isSearchOpen}
+          setIsSearchOpen={setIsSearchOpen}
+          toggleSearch={toggleSearch}
+        />
+      )}
     </HeaderWrapper>
+  );
+};
+const LoggedInHeaderComp: React.FC<HeaderCompProps> = ({
+  userProfile,
+  isSearchPage,
+  isSearchOpen,
+  setIsSearchOpen,
+  toggleSearch,
+}) => {
+  return (
+    <>
+      <div className="wrap">
+        {isSearchPage ? (
+          ""
+        ) : (
+          <>
+            <div className="md-flex">
+              <SearchBar isMainBar={true} setIsSearchOpen={setIsSearchOpen} />
+            </div>
+            <img
+              className="searchIcon md-hidden"
+              src="/search-icon.svg"
+              alt=""
+              onClick={toggleSearch}
+            />
+          </>
+        )}
+        <Link to={`/profile/${userProfile?.userID}`}>
+          <img className="profileImg" src={userProfile?.profileImage || noImg} alt="image" />
+        </Link>
+      </div>
+      {isSearchPage ? (
+        ""
+      ) : (
+        <div className={`searchModal ${isSearchOpen ? "" : "hidden"}`}>
+          <SearchBar isMainBar={false} setIsSearchOpen={setIsSearchOpen} />
+        </div>
+      )}
+    </>
+  );
+};
+const LoggedOutHeaderComp: React.FC<HeaderCompProps> = ({
+  isSearchPage,
+  isSearchOpen,
+  setIsSearchOpen,
+  toggleSearch,
+}) => {
+  return (
+    <>
+      <div className="wrap">
+        {isSearchPage ? (
+          ""
+        ) : (
+          <div className="md-flex">
+            <SearchBar isMainBar={true} setIsSearchOpen={setIsSearchOpen} />
+          </div>
+        )}
+        <div className="btnWrap">
+          <Link to="/login">
+            <Button text="로그인" colorType={3} />
+          </Link>
+          {isSearchPage ? (
+            ""
+          ) : (
+            <img
+              className="searchIcon md-hidden"
+              src="/search-icon.svg"
+              alt=""
+              onClick={toggleSearch}
+            />
+          )}
+        </div>
+      </div>
+      {isSearchPage ? (
+        ""
+      ) : (
+        <div className={`searchModal ${isSearchOpen ? "" : "hidden"}`}>
+          <SearchBar isMainBar={false} setIsSearchOpen={setIsSearchOpen} />
+        </div>
+      )}
+    </>
+  );
+};
+
+const SearchBar = ({
+  isMainBar,
+  setIsSearchOpen,
+}: {
+  isMainBar: boolean;
+  setIsSearchOpen: (b: boolean) => void;
+}) => {
+  const [searchWord, setSearchWord] = useState("");
+  const { setSearchParams } = useStoreSearchPage();
+  const navigate = useNavigate();
+
+  const handleSearch = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+    if (ev.key !== "Enter") return;
+    if (searchWord.trim()) {
+      setSearchParams({ keyword: searchWord });
+      navigate(`/search`);
+
+      console.log(`/search로 리디랙트(keyword:${searchWord})`);
+      setIsSearchOpen(false);
+    }
+  };
+  return (
+    <SearchBarWrapper>
+      <input
+        type="text"
+        className={`searchBar ${isMainBar ? "" : "md-hidden"}`}
+        placeholder="다양한 개발자 포트폴리오 검색"
+        value={searchWord}
+        onChange={ev => setSearchWord(ev.target.value)}
+        onKeyDown={handleSearch}
+      />
+      <img src="/search-icon.svg" className={`searchIcon ${isMainBar ? "" : "md-hidden"}`} alt="" />
+    </SearchBarWrapper>
   );
 };
 export default Header;
