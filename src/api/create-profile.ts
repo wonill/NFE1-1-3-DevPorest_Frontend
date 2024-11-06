03cab6d6181fc83fcc2f339b7ff5a0c154a5a408
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-06 11:23:31 +0900

    ✨ 유저 프로필 수정 API 추가

--- src/api/create-profile.ts
+++ src/api/create-profile.ts
@@ -1,12 +1,26 @@
-import ky from "ky";
+import api from ".";
 import { UserProfileType, UserProfileResType, UserApiResType } from "../types/api-types/UserType";
-const apiUrl = import.meta.env.VITE_SERVER_URL;
 
 export const createProfile = async (userData: UserProfileType) => {
   try {
-    const response = await ky.post(`${apiUrl}/users`, {
+    const response = await api.post(`users`, {
       json: userData,
-      credentials: "include",
+    });
+
+    const result: UserApiResType<UserProfileResType> = await response.json();
+
+    if (result.success) return result.data;
+    else return result.error;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
+
+export const modifyProfile = async (userData: UserProfileType) => {
+  try {
+    const response = await api.put(`users`, {
+      json: userData,
     });
 
     const result: UserApiResType<UserProfileResType> = await response.json();
