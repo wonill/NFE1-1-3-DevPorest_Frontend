3261a43ed3d38c52886491e4b5530931ab2612d8
jooyoung <jykimjbr860@gmail.com>
2024-11-06 15:51:35 +0900

    🐛 포폴 사진비율 고정, 댓글 시간 한국시간으로 변경

--- src/components/CommentBox/CommentBox.tsx
+++ src/components/CommentBox/CommentBox.tsx
@@ -1,41 +1,22 @@
-import { useEffect, useId, useState } from "react";
-// import { CommentResType } from "../../types/api-types/CommentType";
+import { useEffect, useState } from "react";
 import { CommentBoxWrapper } from "./CommentBox.styles";
-import ky from "ky";
 import { UserApiResType, UserProfileResType } from "../../types/api-types/UserType";
 import userApi from "../../api";
-const apiUrl = import.meta.env.VITE_SERVER_URL;
 import noImg from "../../assets/no_image.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface CommentBoxProps {
-  // comment:CommentResType
-  _id: string;
   content: string; // 댓글 내용
   userID: string; // 댓글 작성자 ID
-  portfolioID: string; // 포트폴리오 ID
   createdAt: string; // 생성 날짜
-  // name: string; // 댓 작성자 이름
-  // job: string;
-  // profileImg: string;
-  // writeTime: Date;
-  // commentText: string;
   isMyComment: boolean; // 댓 작성자와 로그인된 사용자가 동일한지
   onClickDelete?: () => void; // x 버튼 활성화된 경우 클릭시 댓글 삭제
 }
 
 const CommentBox: React.FC<CommentBoxProps> = ({
-  // comment
-  _id,
   content,
   userID,
-  portfolioID,
   createdAt,
-  // name,
-  // job,
-  // profileImg,
-  // writeTime,
-  // commentText,
   isMyComment,
   onClickDelete,
 }) => {
@@ -75,6 +56,10 @@ const CommentBox: React.FC<CommentBoxProps> = ({
   useEffect(() => {
     fetchUser();
   }, []);
+  const UTF2KOR = () => {
+    const date = new Date(createdAt);
+    return date.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" });
+  };
 
   return (
     <CommentBoxWrapper>
@@ -91,7 +76,7 @@ const CommentBox: React.FC<CommentBoxProps> = ({
       </div>
       <div className="textBox">
         <p>{content}</p>
-        <p className="writeTime">{createdAt.slice(0, 16).replace("T", " ")}</p>
+        <p className="writeTime">{UTF2KOR().slice(0, -3)}</p>
       </div>
 
       {isMyComment ? (
