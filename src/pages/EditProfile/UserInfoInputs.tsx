71e23a0e7f8218bfdc57b435787f22b59cdb7753
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-06 11:26:20 +0900

    ♻️ UserInfoInputs 컴포넌트 리팩토링 & 버그 수정

--- src/pages/EditProfile/UserInfoInputs.tsx
+++ src/pages/EditProfile/UserInfoInputs.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, SetStateAction } from "react";
 import InfoInput from "./InfoInput";
 import email from "../../assets/email.svg";
 import mobile from "../../assets/mobile.svg";
@@ -6,59 +6,34 @@ import blog from "../../assets/blog.svg";
 import gitHub from "../../assets/github.svg";
 import instagram from "../../assets/instagram.svg";
 import { validateInput } from "../../utils/valid-input";
+import { UserProfileResType } from "../../types/api-types/UserType";
 
 interface UserInfoInputsProps {
   name: string;
-  userInfo?: {
-    email: string;
-    mobile: string;
-    github: string;
-    instagram: string;
-    blog: string;
-  };
-  onChange: (data: {
-    email: string;
-    mobile: string;
-    github: string;
-    instagram: string;
-    blog: string;
-  }) => void;
+  userInfo: UserProfileResType;
+  setUserInfo: React.Dispatch<SetStateAction<UserProfileResType>>;
 }
 
-const UserInfoInputs: React.FC<UserInfoInputsProps> = ({ name, userInfo, onChange }) => {
-  const [emailValue, setEmailValue] = useState("");
-  const [mobileValue, setMobileValue] = useState("");
-  const [githubValue, setGithubValue] = useState("");
-  const [instagramValue, setInstagramValue] = useState("");
-  const [blogValue, setBlogValue] = useState("");
-
+const UserInfoInputs: React.FC<UserInfoInputsProps> = ({ name, userInfo, setUserInfo }) => {
   const [errors, setErrors] = useState({
-    email: false,
-    mobile: false,
-    github: false,
-    instagram: false,
-    blog: false,
+    email: !validateInput("email", userInfo.email),
+    mobile: !validateInput("mobile", userInfo.phoneNumber),
+    github: !validateInput("github", userInfo.links[0]),
+    instagram: !validateInput("url", userInfo.links[1]),
+    blog: !validateInput("url", userInfo.links[2]),
   });
 
   useEffect(() => {
     const newErrors = {
-      email: validateInput("email", emailValue) ? false : true,
-      mobile: validateInput("mobile", mobileValue) ? false : true,
-      github: validateInput("github", githubValue) ? false : true,
-      instagram: validateInput("url", instagramValue) ? false : true,
-      blog: validateInput("url", blogValue) ? false : true,
+      email: !validateInput("email", userInfo.email),
+      mobile: !validateInput("mobile", userInfo.phoneNumber),
+      github: !validateInput("github", userInfo.links[0]),
+      instagram: !validateInput("url", userInfo.links[1]),
+      blog: !validateInput("url", userInfo.links[2]),
     };
 
     setErrors(newErrors);
-
-    onChange({
-      email: emailValue,
-      mobile: mobileValue,
-      github: githubValue,
-      instagram: instagramValue,
-      blog: blogValue,
-    });
-  }, [emailValue, mobileValue, githubValue, instagramValue, blogValue, onChange]);
+  }, [userInfo]);
 
   return (
     <>
@@ -66,39 +41,59 @@ const UserInfoInputs: React.FC<UserInfoInputsProps> = ({ name, userInfo, onChang
       <InfoInput
         icon={email}
         placeholder="이메일을 입력해주세요."
-        value={userInfo?.email!}
-        onChange={e => setEmailValue(e.target.value)}
+        value={userInfo.email}
+        onChange={e =>
+          setUserInfo(prev => {
+            return { ...prev, email: e.target.value };
+          })
+        }
         error={errors.email}
       />
 
       <InfoInput
         icon={mobile}
         placeholder="010-XXXX-XXXX의 형식을 지켜주세요"
-        value={userInfo?.mobile!}
-        onChange={e => setMobileValue(e.target.value)}
+        value={userInfo.phoneNumber}
+        onChange={e =>
+          setUserInfo(prev => {
+            return { ...prev, phoneNumber: e.target.value };
+          })
+        }
         error={errors.mobile}
       />
 
       <InfoInput
         icon={gitHub}
         placeholder="GitHub 링크를 입력해주세요."
-        value={userInfo?.github!}
-        onChange={e => setGithubValue(e.target.value)}
+        value={userInfo?.links[0] || ""}
+        onChange={e =>
+          setUserInfo(prev => {
+            return { ...prev, links: [e.target.value, userInfo.links[1], userInfo.links[2]] };
+          })
+        }
         error={errors.github}
       />
 
       <InfoInput
         icon={instagram}
         placeholder="Instagram 링크를 입력해주세요."
-        value={userInfo?.instagram!}
-        onChange={e => setInstagramValue(e.target.value)}
+        value={userInfo?.links[1] || ""}
+        onChange={e =>
+          setUserInfo(prev => {
+            return { ...prev, links: [userInfo.links[0], e.target.value, userInfo.links[2]] };
+          })
+        }
       />
 
       <InfoInput
         icon={blog}
         placeholder="Blog 링크를 입력해주세요."
-        value={userInfo?.blog!}
-        onChange={e => setBlogValue(e.target.value)}
+        value={userInfo?.links[2] || ""}
+        onChange={e =>
+          setUserInfo(prev => {
+            return { ...prev, links: [userInfo.links[0], userInfo.links[1], e.target.value] };
+          })
+        }
       />
     </>
   );
