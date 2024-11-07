fcc4a24f8a612050a3dd1fcf312578f785738250
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-11-07 15:05:56 +0900

    ✨ 유저 프로필 업로드 API 구현

--- src/api/upload-single-img.ts
+++ src/api/upload-single-img.ts
@@ -21,3 +21,21 @@ export const uploadTumbnailImg = async (
     throw error;
   }
 };
+
+export const uploadProfileImg = async (img: File): Promise<string | undefined> => {
+  const formData = new FormData();
+  formData.append("image", img);
+  try {
+    const response = await api
+      .post(`users/upload?usage=profile`, {
+        body: formData,
+      })
+      .json<PortfolioThumbnailResType>();
+
+    if (response.success) return response.data?.url;
+    throw new Error(response.error);
+  } catch (error) {
+    console.error("단일 이미지 업로드 오류:", error);
+    throw error;
+  }
+};
